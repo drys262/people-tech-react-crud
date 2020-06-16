6fe3371b3a35bfd44cac31814323f3efb96c9f9e
drys262 <koeichavez17@gmail.com>
2020-06-17 00:37:34 +0800

    feat: remove not used files

--- internals/templates/containers/App/index.js
+++ internals/templates/containers/App/index.js
@@ -8,20 +8,14 @@
  */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import { Switch } from 'react-router-dom';
 
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Switch />
       <GlobalStyle />
     </div>
   );
